2e47234a15660bcc2d5a328afffac6def830ba49
Alexandre Rocha <alexandrececchin@gmail.com>
2018-12-13 21:51:12 -0200

    #2 Move state to App.js - make Home stateless - add react-router

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import Dashboard from "../../Components/Dashboard/Dashboard";
+import { Link } from "react-router-dom";
+
+
+const Home = (props) => {
+    return (
+      <div className="app">
+            <div className="list-books">
+              <div className="list-books-title">
+                <h1>MyReads</h1>
+              </div>
+              <div className="list-books-content">
+                <div>
+                  <Dashboard shelfTitle="Currently Reading" booksList={props.reading} />
+                  <Dashboard shelfTitle="Want to Read" booksList={props.booksToRead} />
+                  <Dashboard shelfTitle="Read" booksList={props.booksRead} />
+                </div>
+              </div>
+              <div className="open-search">
+                <Link to="/search">
+                  <button>Add a book</button>
+                </Link>
+              </div>
+            </div>
+      </div>
+    )
+}
+
+Home.prototype = {
+  booksList: PropTypes.array.isRequired,
+  booksToRead: PropTypes.array.isRequired,
+  booksRead: PropTypes.array.isRequired,
+}
+
+export default Home;
