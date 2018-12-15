28ecbf73eaf38eba56b72186a83722fb0964182f
Alexandre Rocha <alexandrececchin@gmail.com>
2018-12-15 10:15:28 -0200

    #5 Implement missing itens from rubric

--- src/Components/Books/Book/Book.js
+++ src/Components/Books/Book/Book.js
@@ -2,12 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Book = props => {
+    const thumbnail = props.book.imageLinks && props.book.imageLinks.thumbnail ? props.book.imageLinks.thumbnail: '';
+    const autor = props.book.authors ? props.book.authors: '';
+    const title = props.book.title ? props.book.title: '';
+    const shelf = props.book.shelf;
     return (
         <div className="book">
             <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${props.book.imageLinks.thumbnail})` }}></div>
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${thumbnail})` }}></div>
                 <div className="book-shelf-changer">
-                    <select value={props.book.shelf} onChange={(e) => props.moveBookToShelf(props.book, e.target.value)}>
+                    <select value={shelf} onChange={(e) => props.moveBookToShelf(props.book, e.target.value)}>
                         <option value="move" disabled>Move to...</option>
                         <option value="currentlyReading">Currently Reading</option>
                         <option value="wantToRead">Want to Read</option>
@@ -16,8 +20,8 @@ const Book = props => {
                     </select>
                 </div>
             </div>
-            <div className="book-title">{props.book.title}</div>
-            <div className="book-authors">{props.book.authors}</div>
+            <div className="book-title">{title}</div>
+            <div className="book-authors">{autor}</div>
         </div>
     );
 };
