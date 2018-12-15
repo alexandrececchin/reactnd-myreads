1c2989726b80398d2f339a3c47e0783585304634
Alexandre Rocha <alexandrececchin@gmail.com>
2018-12-14 23:05:24 -0200

    #4  Add loading and messagens of item not found

--- src/App.js
+++ src/App.js
@@ -8,10 +8,9 @@ import Home from "./Components/Home/Home";
 class BooksApp extends React.Component {
   state = {
     books: [],
-    reading: [],
-    booksToRead: [],
-    booksRead: [],
-    query: ''
+    query: '',
+    showLoad: '',
+    errorMsg: ''
   }
 
   componentDidMount() {
@@ -19,8 +18,9 @@ class BooksApp extends React.Component {
   }
 
   fetchAllBooks = () => {
+    this.showLoader();
     api.getAll().then((books) => {
-      this.setState({ books });
+      this.setState({ books }, function a() { this.removeLoader(); });
     });
   }
 
@@ -29,28 +29,46 @@ class BooksApp extends React.Component {
   }
 
   search = () => {
-    console.log('Search by ' + this.state.query)
-    api.search(this.state.query).then((res) => this.setState({ books: res }));
+    this.showLoader();
+    api.search(this.state.query).then(res => {
+      if (Array.isArray(res)) {
+        this.setState({ books: res, errorMsg: '' }, function a() { this.removeLoader() });
+      } else if (res.error) {
+        this.setState({ books: [], errorMsg: '0 Items found' }, function a() { this.removeLoader() });
+      }
+    });
   }
 
   moveBookToShelf = (book, shelf) => {
+    this.showLoader();
     api.update(book, shelf).then((res) => this.fetchAllBooks());
   }
 
+  showLoader = () => {
+    this.setState({ showLoad: 'active' })
+  }
+
+  removeLoader = () => {
+    this.setState({ showLoad: '' })
+  }
 
   render() {
     const booksRead = this.state.books.filter(books => books.shelf === 'read');
     const booksToRead = this.state.books.filter(books => books.shelf === 'wantToRead');
     const reading = this.state.books.filter(books => books.shelf === 'currentlyReading');
+
     return (
       <div className="app">
-        <div className="loader2">
-          <div className="ui active inline loader" style={{ display: 'none' }} />
+        <div className={`ui ${this.state.showLoad} dimmer indeterminate`}>
+          <div className="ui big text loader">Loading...</div>
         </div>
         <Switch>
-          <Route exact path="/search" render={() => (<Search booksList={this.state.books} fetchAllBooks={this.fetchAllBooks} moveBookToShelf={this.moveBookToShelf}
-            query={this.state.query} queryChange={this.queryChangeHandler} search={this.search} />)} />
-          <Route exact path="/" render={() => (<Home reading={reading} booksToRead={booksToRead} booksRead={booksRead} moveBookToShelf={this.moveBookToShelf} />)} />
+          <Route exact path="/search" render={() => (<Search booksList={this.state.books} fetchAllBooks={this.fetchAllBooks}
+            errorMsg={this.state.errorMsg} moveBookToShelf={this.moveBookToShelf} query={this.state.query}
+            queryChange={this.queryChangeHandler} search={this.search} />)} />
+
+          <Route exact path="/" render={() => (<Home reading={reading} booksToRead={booksToRead} booksRead={booksRead}
+            moveBookToShelf={this.moveBookToShelf} />)} />
         </Switch>
       </div>
     )
--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -13,22 +13,17 @@ const Search = props => {
                 </Link>
 
                 <div className="search-books-input-wrapper">
-                    {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
-                    <input type="text" style={{ width: '90%' }} placeholder="Search by title or author" value={props.value} onChange={(event)=> props.queryChange(event.target.value)}/>
+                    <input type="text" style={{ width: '90%' }} placeholder="Search by title or author" value={props.value} onChange={(event) => props.queryChange(event.target.value)} />
                     <button className="ui primary button" onClick={() => props.search()} disabled={props.value === ''}>
                         <i className="search icon" />
                     </button>
                 </div>
             </div>
             <div className="search-books-results">
-                <Dashboard shelfTitle="Result" booksList={props.booksList} moveBookToShelf={props.moveBookToShelf} />
+                {props.errorMsg === '' ?
+                    <Dashboard shelfTitle="Result" booksList={props.booksList} moveBookToShelf={props.moveBookToShelf} />
+                    : <h2 style={{textAlign: 'center'}}><strong>{props.errorMsg}</strong></h2>
+                }
             </div>
         </div>
     );
